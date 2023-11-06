e07cca3c2d1c3cb57a315a05276068d083df5896
minifear69 <123888006+shaongaem@users.noreply.github.com>
2023-11-06 11:43:43 -0800

    Adds the rest of the Nkhook6 source code files via upload the rest of them

---
+++ Bloons_Impl/Implementations/Game/NGameInstance.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Assets.Scripts.Unity.UI_New.InGame;
+using NKHook6.API.Game;
+
+namespace KHook6_Impl.Implementations.Game
+{
+	// Token: 0x0200002E RID: 46
+	public class NGameInstance : IGameInstance
+	{
+		// Token: 0x0600006F RID: 111 RVA: 0x00002816 File Offset: 0x00000A16
+		public static NGameInstance GetGame()
+		{
+			if (NGameInstance.gameInstance == null)
+			{
+				InGame instance = InGame.instance;
+				if (instance == null)
+				{
+					throw new Exception("Cannot get NGameInstance without being in a game!");
+				}
+				NGameInstance.gameInstance = new NGameInstance(instance);
+			}
+			return NGameInstance.gameInstance;
+		}
+
+		// Token: 0x06000070 RID: 112 RVA: 0x00002847 File Offset: 0x00000A47
+		private NGameInstance(InGame inGame)
+		{
+			this.inGame = inGame;
+		}
+
+		// Token: 0x06000071 RID: 113 RVA: 0x00002856 File Offset: 0x00000A56
+		public InGame GetNative()
+		{
+			return this.inGame;
+		}
+
+		// Token: 0x06000072 RID: 114 RVA: 0x0000285E File Offset: 0x00000A5E
+		public double GetHealth()
+		{
+			return (double)this.inGame.bridge.GetHealth();
+		}
+
+		// Token: 0x06000073 RID: 115 RVA: 0x0000271A File Offset: 0x0000091A
+		public IMap GetMap()
+		{
+			throw new NotImplementedException();
+		}
+
+		// Token: 0x06000074 RID: 116 RVA: 0x0000271A File Offset: 0x0000091A
+		public IPlayer GetPlayer()
+		{
+			throw new NotImplementedException();
+		}
+
+		// Token: 0x06000075 RID: 117 RVA: 0x0000271A File Offset: 0x0000091A
+		public IPlayer GetPlayer(int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Token: 0x06000076 RID: 118 RVA: 0x00002871 File Offset: 0x00000A71
+		public void Lose()
+		{
+			this.inGame.Loose();
+		}
+
+		// Token: 0x06000077 RID: 119 RVA: 0x0000271A File Offset: 0x0000091A
+		public void SetHealth(double value)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Token: 0x0400001E RID: 30
+		private static NGameInstance gameInstance;
+
+		// Token: 0x0400001F RID: 31
+		private InGame inGame;
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using MelonLoader;
+using NKHook6_Impl;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("NKHook6_Impl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NKHook6_Impl")]
+[assembly: AssemblyCopyright("Copyright © NKHook 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("796FD8F7-95F0-4FDE-9EBD-1574DB80EC3A")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+[assembly: MelonInfo(typeof(Entry), "NKHook6-Impl", "0.0.1", "NKHook Team", null)]
